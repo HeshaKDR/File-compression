bfe538abbd7b5645f09a0fb3a633c67d12dfe711
HeshaKDR <hesha@DESKTOP-IFPVLQD>
2024-10-11 18:02:00 +0300

    now it comprecess propperly

--- src/huffman.c
+++ src/huffman.c
@@ -62,62 +62,111 @@ void construct_huffman(unsigned *freq_in, HuffNode *tree) {
   node[0]->parent = NULL;
 }
 
+void tree_bit_encoding(HuffNode *node, FILE *fout, unsigned char *buffer, int *bit_count){
+  if(node->is_leaf){
+    *buffer |= (0 << *bit_count);
+    (*bit_count)++;
+    if(*bit_count == 8){
+      
+      fputc(*buffer, fout);
+      *buffer = 0;
+      bit_count = 0;
+      }
+      for(int i = 7; i >= 0; i--){
+        *buffer |= ((node->data >> i) & 1) << *bit_count;
+        (*bit_count)++;
+        if(*bit_count == 8){
+          
+          fputc(*buffer, fout);
+          *buffer = 0;
+          *bit_count = 0;        }
+      }
+  }
+  else{
+    *buffer |= (1 << *bit_count);
+    (*bit_count) ++;
+    if(*bit_count == 8){
+      fputc(*buffer, fout);
+      *buffer = 0;
+      *bit_count = 0;
+
+    }
+    tree_bit_encoding(node->left, fout, buffer, bit_count);
+    tree_bit_encoding(node->right, fout, buffer, bit_count);
+  }
+}
+
+
+
 void encode_stream(FILE *fin, FILE *fout, HuffNode *tree, unsigned *padding) {
-  int n;
-  unsigned char ch;
-  unsigned char buff = 0, nbuf = 0;
+  int ch;
+  unsigned char buffer = 0;
+  int bit_count = 0;
   HuffNode *p;
   unsigned char code[256] = {0};
-  while ((n = fgetc(fin)) != EOF) {
-    if(n < 0 || n >= 256){
-      printf("invalid characters in the file");
-      return;
-    }
-    ch = n;
+  while ((ch = fgetc(fin)) != EOF) {
+    if(ch >= 0 && ch < 256){
+      
+    
     p = &tree[ch];
+    
+    
     int code_length = 0;
     while (p->parent) {
       if (p == p->parent->right) {
         code[code_length] = 1;
       }
+      else{
+        code[code_length] = 0;
+      }
       p = p->parent;
       code_length++;
     }
+
     for (int i = code_length - 1; i >= 0; i--) {
-      buff |= code[i] << nbuf;
-      nbuf++;
-      if (nbuf == 8) {
-        fputc(buff, fout);
-        nbuf = 0;
-        buff = 0;
+
+      buffer |= code[i] << bit_count;
+      
+      bit_count++;
+      if (bit_count == 8) {
+        
+        fputc(buffer, fout);
+        bit_count = 0;
+        buffer = 0;
       }
     }
+  } else{
+    printf("invalid characters in the file");
+      return;
+    
   }
-  if (nbuf > 0) {
-    fputc(buff, fout);
+  
+}
+if (bit_count > 0) {
+    fputc(buffer, fout);
   }
-  *padding = 8 - nbuf;
+  *padding = 8 - bit_count;
 }
 
 void compress_file(FILE *fin, FILE *fout) {
 
   unsigned freq[256] = {0}, padding = 0; //- хз
-  int flag = 0;
-  size_t padding_pos;
-  HuffNode tree[512]; //нахуя тут нужно выделение памяти под дерево если выделяется память под сами узлы...
   
-  if (flag == 0) {
+  unsigned char buffer = 0;
+  int bit_count = 0;
+  HuffNode tree[512];
+
+  
     count_frequency(fin, freq);
     construct_huffman(freq, tree);
     rewind(fin);
-    for (int i = 0; i < 256; i++) {
-      fwrite(&freq[i], sizeof(unsigned), 1, fout);
-    }
-    padding_pos = ftell(fout);
-    fwrite(&padding, sizeof(unsigned), 1, fout);
-    encode_stream(fin, fout, tree, &padding);
-    fseek(fout, padding_pos, SEEK_SET);
-    fwrite(&padding, sizeof(unsigned), 1, fout);
+    tree_bit_encoding(&tree[511], fout, &buffer, &bit_count);
     
+    if(bit_count > 0){
+      fputc(buffer, fout);
+    }
+    encode_stream(fin, fout, tree, &padding);
+    
+    fwrite(&padding, sizeof(unsigned), 1, fout);
+
   }
-}
